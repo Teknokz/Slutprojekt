5374c13a4b7e48c935460cd0ba2cba0e2cf856bb
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-03-11 13:24:09 +0100

    Added Operator index
    Also started the grid for choosing operator or defender

--- Program.cs
+++ Program.cs
@@ -10,7 +10,101 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
+            string[] Map =
+                {"Defender{}",
+                 "Attacker{}"};
+            string[] Defenders =
+                {"Smoke",
+                 "Mute",
+                 "Castle",
+                 "Pulse",
+                 "Doc",
+                 "Rook",
+                 "Kapkan",
+                 "Tachanka",
+                 "Jäger",
+                 "Bandit",
+                 "Frost",
+                 "Valkyrie",
+                 "Caveira",
+                 "Echo",
+                 "Mira",
+                 "Lesion",
+                 "Ela",
+                 "Vigil",
+                 "Maestro",
+                 "Alibi",
+                 "Clash",
+                 "Kaid",
+                 "Mozzy",
+                 "Warden",
+                 "Goyo",
+                 "Wamai",
+                 "Oryx"
+                 
+            };
+
+            string[] Attackers =
+                {
+                 "Sledge",
+                 "Thatcher",
+                 "Ash",
+                 "Thermite",
+                 "Twitch",
+                 "Montagne",
+                 "Glaz",
+                 "Fuze",
+                 "Blitz",
+                 "IQ",
+                 "Buck",
+                 "Blackbeard",
+                 "Capitão",
+                 "Hibana",
+                 "Jackal",
+                 "Ying",
+                 "Zofia",
+                 "Dokkaebi",
+                 "Lion",
+                 "Finka",
+                 "Maverick",
+                 "Nomad",
+                 "Gridlock",
+                 "NØkk",
+                 "Amaru <3",
+                 "Kali",
+                 "Iana"
+            };
+
             Console.WriteLine("Welcome to Rainbow operator picker!");
+
+            Console.WriteLine("Defend or Attack?");
+
+            int menuPosition = 0;
+            //
+            while(1 == 1)
+            {
+                if(menuPosition == 0)
+                {
+                    Position();
+
+                }
+                else
+                {
+
+                }
+            }
+
+
+
+
+            Console.ReadLine();
         }
+       private static int Position()
+        {
+            ConsoleKeyInfo ValidKeys = Console.ReadKey(true);
+
+            int i = 0;
+            return i;
+        } 
     }
 }
