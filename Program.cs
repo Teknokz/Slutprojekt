bb170f80ad91945a3a31cd5ea05ab12138482225
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-03-23 09:33:45 +0100

    Menu worrk started

--- Program.cs
+++ Program.cs
@@ -10,9 +10,6 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
-            string[] Map =
-                {"Defender{}",
-                 "Attacker{}"};
             string[] Defenders =
                 {"Smoke",
                  "Mute",
@@ -41,7 +38,7 @@ namespace Slutprojekt
                  "Goyo",
                  "Wamai",
                  "Oryx"
-                 
+
             };
 
             string[] Attackers =
@@ -75,36 +72,73 @@ namespace Slutprojekt
                  "Iana"
             };
 
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
             Console.WriteLine("Welcome to Rainbow operator picker!");
 
             Console.WriteLine("Defend or Attack?");
 
-            int menuPosition = 0;
-            //
-            while(1 == 1)
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Defend or Attack?");
+            Console.WriteLine("1) Attack");
+            Console.WriteLine("2) Defend");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
             {
-                if(menuPosition == 0)
-                {
-                    Position();
-
-                }
-                else
-                {
-
-                }
+                case "1":
+                    Attack();
+                    return true;
+                case "2":
+                    Defend();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
             }
+        }
 
+        private static string CaptureInput()
+        {
+            Console.Write("Enter the string you want to modify: ");
+            return Console.ReadLine();
+        }
 
+        private static void Attack()
+        {
+            Console.Clear();
+            Console.WriteLine("You have chosen Attackers:");
 
+            char[] charArray = CaptureInput().ToCharArray();
+            Array.Reverse(charArray);
+            DisplayResult(String.Concat(charArray));
+        }
 
+        private static void Defend()
+        {
+            Console.Clear();
+            Console.WriteLine("You have chosen Defenders:");
+
+            DisplayResult(CaptureInput().Replace(" ", ""));
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.Write("\r\nPress Enter to return to Main Menu");
             Console.ReadLine();
         }
-       private static int Position()
-        {
-            ConsoleKeyInfo ValidKeys = Console.ReadKey(true);
-
-            int i = 0;
-            return i;
-        } 
     }
 }
+    
+
